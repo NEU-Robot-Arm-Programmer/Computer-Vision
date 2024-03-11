1e09463f2773869e45e93f84066441d1e69e213d
Stephen Sodipo <108195516+SMcQ618@users.noreply.github.com>
2024-03-10 22:55:40 -0400

    Worked out configuration
    Fixed the weird errors I was getting with the camera. Also added the angle calculator (can be worked into the 5 finger tracker) and shows the angles on the screen of the window.

---
+++ Main2.py
@@ -0,0 +1,185 @@
+import cv2
+import mediapipe as mp
+import time
+import numpy as np
+
+class handdetec:
+    def __init__(self, mode=False, MaxHands=2, modelComplex=1, detection=0.5, tracking=0.5):
+        self.mode = mode
+        self.MaxHands = MaxHands
+        self.complex = modelComplex
+        self.detection = detection
+        self.track = tracking
+
+        # detecting the landmarks
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.MaxHands, modelComplex, self.detection, tracking)
+        self.mpdraw = mp.solutions.drawing_utils
+
+    # job is to find the hands
+    def find_hands(self, img, draw=True):
+        """Identifies what the hand is in front of teh camera then draws points on it"""
+        self.RGBimg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.result = self.hands.process(self.RGBimg)
+
+
+        if self.result.multi_hand_landmarks:
+            for hand_landmarks in self.result.multi_hand_landmarks:
+                if draw:
+                    thumb_tip = (int(hand_landmarks.landmark[4].x * img.shape[1]), int(hand_landmarks.landmark[4].y * img.shape[0]))
+                    fingers_center = (int((hand_landmarks.landmark[8].x + hand_landmarks.landmark[12].x +
+                                           hand_landmarks.landmark[16].x + hand_landmarks.landmark[20].x) / 4 *
+                                          img.shape[1]),
+                                      int((hand_landmarks.landmark[8].y + hand_landmarks.landmark[12].y +
+                                           hand_landmarks.landmark[16].y + hand_landmarks.landmark[20].y) / 4 *
+                                          img.shape[0]))
+                    wrist = (int(hand_landmarks.landmark[0].x * img.shape[1]), int(hand_landmarks.landmark[0].y * img.shape[0]))
+
+                    angles = self.calculate_angles(hand_landmarks)
+                    for i, angle in enumerate(angles.values()):
+                        #check if the angle is a float
+                        if isinstance(angle, float):
+                            # draw angle near the wrist
+                            cv2.putText(img, f'Angle {i + 1}: {angle:.2f}',
+                                        (10, img.shape[0] - 20 * (len(angles) - i)), #this places the text under fps
+                                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
+
+                    #Draws the points on fingers, thumb, and wrisr
+                    cv2.circle(img, thumb_tip, 20, (255, 0, 0), cv2.FILLED)
+                    cv2.circle(img, fingers_center, 25, (255, 0, 0), cv2.FILLED)
+                    cv2.circle(img, wrist, 20, (0, 0, 255), cv2.FILLED)
+
+                    #draws lines to connect the points
+                    cv2.line(img, wrist, thumb_tip, (255, 255, 255), 3)  # Line from wrist to thumb
+                    cv2.line(img, wrist, fingers_center, (255, 255, 255), 3)  # Line from wrist to fingers center
+                    cv2.line(img, thumb_tip, fingers_center, (255, 255, 255), 3)  # Line from thumb to fingers center
+
+        return img
+
+
+    # find the positions (we'll use to then output)
+    def find_position(self, img, handNum=0):
+        """Finds the position of the hand from the points on the hand"""
+        lmlist = []
+
+        if self.result.multi_hand_landmarks:
+            myHand = self.result.multi_hand_landmarks[handNum]
+
+            #coordinates fo the origin
+            origin_x, origin_y, origin_z = 0.0, 0.0, 0.0
+
+            physical_width = 1.0 #example in meters
+            physical_height = 1.0
+            physical_depth = 1.0
+            resolution_width, resolution_height, resolution_depth = img.shape[0], img.shape[1], img.shape[2]
+
+            for id, lm in enumerate(myHand.landmark):
+                if id in [0, 4, 8, 12, 16, 20]:
+                    cx, cy, cz = int(lm.x * img.shape[1]), int(lm.y * img.shape[0]), 0  # assuming depth is 0
+
+                    real_world_x, real_world_y, real_world_z = pixels_to_meters(cx, cy, cz, physical_width,
+                                                                                physical_height, resolution_width,
+                                                                                resolution_height, resolution_depth)
+
+                    relative_x = real_world_x - origin_x
+                    # these are from debugging print(relative_x)
+                    relative_y = real_world_y - origin_y
+                    #print(relative_y)
+                    relative_z = real_world_z - origin_z
+                    #print(relative_z)
+                    lmlist.append([id, relative_x, relative_y, relative_z])
+
+        return lmlist
+
+    def calculate_angles(self, hand_landmarks):
+        """Calculates the angles that the hand is moving on camera"""
+        angles = {}
+
+        finger_indicies = [4, 8, 12, 16, 20] #Thumb, index, middle, ring, pinky
+        palm_indicies = [0, 1, 5, 9, 13, 17] #from teh wrist to the joints of the fingers
+
+        #calc angles between palm and finger??
+        for finger_indx in range(len(finger_indicies)):
+            tip = finger_indicies[finger_indx]
+            base = palm_indicies[finger_indx]
+
+            tip_landmark = hand_landmarks.landmark[tip]
+            base_landmark = hand_landmarks.landmark[base]
+            wrist_landmark = hand_landmarks.landmark[0]
+
+            tip_vector = np.array([tip_landmark.x, tip_landmark.y, tip_landmark.z])
+            base_vector = np.array([base_landmark.x, base_landmark.y, base_landmark.z])
+            wrist_vector = np.array([wrist_landmark.x, wrist_landmark.y, wrist_landmark.z])
+
+            #calculate teh angle between teh finger vectors and the wrists
+            finger_wrist_vector = wrist_vector - base_vector
+            dot_product = np.dot(tip_vector - base_vector, finger_wrist_vector)
+            magnitude_prod = np.linalg.norm(tip_vector - base_vector) * np.linalg.norm(finger_wrist_vector)
+
+            if magnitude_prod != 0:
+                cos_ang = dot_product / magnitude_prod
+                #need to avoid invalid input
+                cos_ang = max(min(cos_ang, 1), -1)
+                angle = np.arccos(cos_ang)
+                angles[f'Finger_{finger_indx} + 1'] = np.degrees(angle)
+            else:
+                angles[f'Finger_{finger_indx + 1}'] = np.nan
+
+        #calculate the angle of the wrist flex/extend
+        wrist_tip = np.array([hand_landmarks.landmark[20].x, hand_landmarks.landmark[20].y, hand_landmarks.landmark[20].z])
+        wrist_base = np.array([hand_landmarks.landmark[0].x, hand_landmarks.landmark[0].y, hand_landmarks.landmark[0].z])
+
+        wrist_vector = wrist_tip - wrist_base
+        reference_vector = np.array([0, 0, 1])  # Assuming z-axis is upward
+        wrist_angle = np.arccos(np.dot(wrist_vector, reference_vector) / (
+                    np.linalg.norm(wrist_vector) * np.linalg.norm(reference_vector)))
+        angles['Wrist_Flexion_Extension'] = np.degrees(wrist_angle)
+
+        return angles
+
+def pixels_to_meters(pixel_x, pixel_y, pixel_z, physical_width, physical_height,
+                     resolution_width, resolution_height, resolution_depth):
+    """ This converts the pixel output into meters"""
+    pixel_size_horizontal = physical_width / resolution_width
+    pixel_size_veritcal = physical_height / resolution_height
+    pixel_size_depth = physical_width / resolution_depth
+
+    real_world_x = pixel_x * pixel_size_horizontal
+    real_world_y = pixel_y * pixel_size_veritcal
+    real_world_z = pixel_z * pixel_size_depth
+
+    return real_world_x, real_world_y, real_world_z
+
+
+def main():
+    cTime = 0
+    pTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = handdetec()
+
+    while True:
+        success, img = cap.read()
+        # class object
+        detector = handdetec()
+        img = detector.find_hands(img)
+        lm_list = detector.find_position(img)
+        if len(lm_list) != 0:
+            print(lm_list)
+
+        # calculating time
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, f'FPS: {int(fps)}', (10, 70), cv2.FONT_HERSHEY_PLAIN,
+                    1, (255, 0, 255), 1)
+
+        cv2.imshow('Hand Tracking', img) #Window name
+        if cv2.waitKey(20) & 0xFF == ord('q'):  #this will close the window
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
